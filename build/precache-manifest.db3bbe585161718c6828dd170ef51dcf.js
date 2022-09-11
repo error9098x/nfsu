d16f398bff3a06ddae6366fb1cb031da10c96433
error9098x <43810146+error9098x@users.noreply.github.com>
2022-09-11 12:46:17 +0530

    Add files via upload

---
+++ build/precache-manifest.db3bbe585161718c6828dd170ef51dcf.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4332abcf12338f34244",
+    "url": "/NFSUCODINGCLUB/static/css/main.271bc6a3.chunk.css"
+  },
+  {
+    "revision": "c4332abcf12338f34244",
+    "url": "/NFSUCODINGCLUB/static/js/main.c4332abc.chunk.js"
+  },
+  {
+    "revision": "90ecf6b5710b2396cef7",
+    "url": "/NFSUCODINGCLUB/static/js/1.90ecf6b5.chunk.js"
+  },
+  {
+    "revision": "4f41e1b4d9a6d0275b56",
+    "url": "/NFSUCODINGCLUB/static/css/2.ccb2e840.chunk.css"
+  },
+  {
+    "revision": "4f41e1b4d9a6d0275b56",
+    "url": "/NFSUCODINGCLUB/static/js/2.4f41e1b4.chunk.js"
+  },
+  {
+    "revision": "f22bfc78a1e06ae8548c",
+    "url": "/NFSUCODINGCLUB/static/js/runtime~main.f22bfc78.js"
+  },
+  {
+    "revision": "d9a1531e7b5524475262cf44c93ed8b3",
+    "url": "/NFSUCODINGCLUB/static/media/meter1.d9a1531e.svg"
+  },
+  {
+    "revision": "70af9240bb95dd2d65f6bf9f09591507",
+    "url": "/NFSUCODINGCLUB/static/media/project-img1.70af9240.png"
+  },
+  {
+    "revision": "98b3bc237950d6cb1a1e49f75372ae36",
+    "url": "/NFSUCODINGCLUB/static/media/project-img2.98b3bc23.png"
+  },
+  {
+    "revision": "08e5b04289e11fe1c0776cef35c4bcf2",
+    "url": "/NFSUCODINGCLUB/static/media/project-img3.08e5b042.png"
+  },
+  {
+    "revision": "1c92f9ac36ed078bc25de452806fe3e3",
+    "url": "/NFSUCODINGCLUB/static/media/logo.1c92f9ac.svg"
+  },
+  {
+    "revision": "dff2b6246274897c980cd8a354f7f979",
+    "url": "/NFSUCODINGCLUB/static/media/nav-icon1.dff2b624.svg"
+  },
+  {
+    "revision": "0393a6c85ddc105c9304f0f7787ddaea",
+    "url": "/NFSUCODINGCLUB/static/media/nav-icon2.0393a6c8.svg"
+  },
+  {
+    "revision": "81ff4e970a39053a0acdc10fd58dc5c4",
+    "url": "/NFSUCODINGCLUB/static/media/nav-icon3.81ff4e97.svg"
+  },
+  {
+    "revision": "b6dee864f54cf83c870833b78093190d",
+    "url": "/NFSUCODINGCLUB/static/media/header-img.b6dee864.svg"
+  },
+  {
+    "revision": "d9a1531e7b5524475262cf44c93ed8b3",
+    "url": "/NFSUCODINGCLUB/static/media/meter2.d9a1531e.svg"
+  },
+  {
+    "revision": "d64094774a77a02c2c22356681131cb8",
+    "url": "/NFSUCODINGCLUB/static/media/color-sharp.d6409477.png"
+  },
+  {
+    "revision": "afc93ad41a3f8bb4403d8a483109ddd1",
+    "url": "/NFSUCODINGCLUB/static/media/color-sharp2.afc93ad4.png"
+  },
+  {
+    "revision": "a8822e204211be92b2412ccbe284e785",
+    "url": "/NFSUCODINGCLUB/static/media/contact-img.a8822e20.svg"
+  },
+  {
+    "revision": "e9228c17c13a87653f1b8ed01a3ff306",
+    "url": "/NFSUCODINGCLUB/static/media/footer-bg.e9228c17.png"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/NFSUCODINGCLUB/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/NFSUCODINGCLUB/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/NFSUCODINGCLUB/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "8ef14aa04e3d62da39e5a3cece40a766",
+    "url": "/NFSUCODINGCLUB/static/media/banner-bg.8ef14aa0.png"
+  },
+  {
+    "revision": "d9a1531e7b5524475262cf44c93ed8b3",
+    "url": "/NFSUCODINGCLUB/static/media/meter3.d9a1531e.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/NFSUCODINGCLUB/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/NFSUCODINGCLUB/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/NFSUCODINGCLUB/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "f750170aaddf4e5f885a27d391a311d9",
+    "url": "/NFSUCODINGCLUB/static/media/arrow1.f750170a.svg"
+  },
+  {
+    "revision": "9070490babe4d1aa7f077b104555b363",
+    "url": "/NFSUCODINGCLUB/static/media/arrow2.9070490b.svg"
+  },
+  {
+    "revision": "6a4075674bab3d6968333f4f62d76402",
+    "url": "/NFSUCODINGCLUB/index.html"
+  }
+];
---
+++ src/components/Projects.js
@@ -0,0 +1,66 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+
+import React from 'react'
+
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>About Us</h2>
+                <p>We are the students of B.Tech-M.Tech (2nd Year) trying to create a coding community</p>
+                
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
